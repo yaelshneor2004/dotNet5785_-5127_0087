864d158319218a05925a8b8570f585fce9e6e026
yaelshneor2004 <yashneor@g.jct.ac.il>
2024-11-06 09:22:53 +0200

    try

---
+++ Stage0/Program5127.cs
@@ -0,0 +1,21 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome5127();
+        Welcome0087();
+        Console.ReadKey();
+    }
+
+
+    private static void Welcome5127()
+    {
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0},welcome to my first console application", name);
+    }
+    private static void Welcome0087()
+    {
+
+    }
+}
