95c85ffdbcb5881f4253f8466586a9cd8c967f11
yaelshneor2004 <yashneor@g.jct.ac.il>
2024-11-06 09:27:15 +0200

    hi

--- Stage0/Program5127.cs
+++ Stage0/Program5127.cs
@@ -11,7 +11,7 @@
     private static void Welcome5127()
     {
         Console.WriteLine("Enter your name");
-        string name = Console.ReadLine();
+        string name = Console.ReadLine(); 
         Console.WriteLine("{0},welcome to my first console application", name);
     }
     private static void Welcome0087()
