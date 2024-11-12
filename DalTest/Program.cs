3b3fe77c051a295d7bd2bb8cf24ceb8ed8fcdbb9
yaelshneor2004 <yashneor@g.jct.ac.il>
2024-11-13 00:04:41 +0200

    jbhj

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -6,7 +6,7 @@ namespace DalTest;
 internal class Program
 {
    public static void Main(string[] args) 
-    {
+    { 
     //    private static IAssignment? s_dalAssignment = new AssignmentImplementation(); //stage 1
     //private static IVolunteer? s_dalVolunteer = new VolunteerImplementation(); //stage 1
     //private static ICall? s_dalCall = new CallImplementation(); //stage 1
