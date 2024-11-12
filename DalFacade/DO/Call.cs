ce3d1ac2a34ae0e85cf657758f0103d6637ff276
ayalaozeri05 <aozeri@g.jct.ac.il>
2024-11-13 01:22:36 +0200

    Notes and documentation stage1

--- DalFacade/DO/Call.cs
+++ DalFacade/DO/Call.cs
@@ -2,7 +2,17 @@
 using Microsoft.VisualBasic;
 
 namespace DO;
-
+/// <summary>
+/// Details and features that each call has
+/// </summary>
+/// <param name="Id">represents a standard ID that uniquely identifies the volunteer</param>
+/// <param name="CallType">according to the specific system type</param>
+/// <param name="Address">Full and real address in correct format, of the reading location</param>
+/// <param name="Latitude">a number that indicates how far a point on Earth is south or north of the equator</param>
+/// <param name="Longitude">a number that indicates how far a point on Earth is south or north of the equator</param>
+/// <param name="OpenTime">Time (date and time) when the call was opened by the manager</param>
+/// <param name="Description">Description of the reading. Detailed details about the call</param>
+/// <param name="MaxFinishCall">Time (date and time) by which the reading should be closed</param>
 public record Call
  (
  int Id,
--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,20 +1,38 @@
 ﻿namespace DO;
+/// <summary>
+/// Enum representing roles within the organization.
+/// Possible values are 'Volunteer' and 'Manager'
+/// </summary>
 public enum MyRole
 {
     Volunteer,
     Manager
 }
+
+/// <summary>
+/// Enum specifying types of distance calculations or travel modes.
+/// Values include 'Aerial' for air distance, 'Walking', and 'Traveling' for vehicle-based movement
+/// </summary>
 public enum MyTypeDistance
 {
     Aerial,
     Walking,
     Traveling
 }
+
+/// <summary>
+/// Enum for categorizing call types in the system. 
+/// </summary>
 public enum MyCallType
 {
     //todo
 }
 
+/// <summary>
+///  Enum representing completion or cancellation statuses for calls.
+/// Options include 'Treated' for completed cases, 'SelfCancel' for user-initiated cancellations, 
+/// 'ManagerCancel' for administrative cancellations, and 'ExpiredCancel' for time-out cases
+/// </summary>
 public enum MyFinishType
 {
     Treated,
