36b2950e988d01d4b4996c5659c2619f5c836907
ayalaozeri05 <aozeri@g.jct.ac.il>
2024-11-17 16:25:50 +0200

    callLatitudes, callLongitudes and addresses

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -42,9 +42,27 @@ private static void createVolunteer()
     {
         string[] volunteerNames =
        { "Dani Levy", "Eli Amar", "Yair Cohen", "Ariela Levin", "Dina Klein", "Shira Israelof", "David Ben-Gurion", "Golda Meir", "Yitzhak Rabin", "Chaim Weizmann", "Menachem Begin", "Ariel Sharon", "Moshe Dayan", "Shimon Peres", "Yigal Alon" };
-        string[] addresses = { "Rothschild 10, Tel Aviv, Israel", "Yigal Alon 120, Tel Aviv, Israel", "Ibn Gvirol 30, Tel Aviv, Israel", "Derech Hevron 78, Jerusalem, Israel", "Herzl Blvd 103, Jerusalem, Israel", "Jaffa St 45, Jerusalem, Israel", "Herzl 9, Haifa, Israel", "Moriah Blvd 70, Haifa, Israel", "The Prophets 11, Haifa, Israel", "Rav Shauli 5, Be'er Sheva, Israel", "Reger Blvd 55, Be'er Sheva, Israel", "Johanna Jabotinsky 15, Be'er Sheva, Israel", "Begin Way 234, Petah Tikva, Israel", "Weizmann 22, Petah Tikva, Israel", "Eli Cohen 1, Ramat Gan, Israel" };
-        double[] Latitude = { 3.2,5.4 };
-        double[] Longitude = { 7.2,1.5 };
+        string[] addresses =  { "Rothschild 10, Tel Aviv, Israel", "Yigal Alon 120, Tel Aviv, Israel", "Ibn Gvirol 30, Tel Aviv, Israel",
+    "Derech Hevron 78, Jerusalem, Israel", "Herzl Blvd 103, Jerusalem, Israel", "Jaffa St 45, Jerusalem, Israel",
+    "Herzl 9, Haifa, Israel", "Moriah Blvd 70, Haifa, Israel", "The Prophets 11, Haifa, Israel",
+    "Rav Shauli 5, Be'er Sheva, Israel", "Reger Blvd 55, Be'er Sheva, Israel", "Johanna Jabotinsky 15, Be'er Sheva, Israel",
+    "Begin Way 234, Petah Tikva, Israel", "Weizmann 22, Petah Tikva, Israel", "Eli Cohen 1, Ramat Gan, Israel",
+    "Shazar Blvd 20, Holon, Israel", "Ben Yehuda 35, Netanya, Israel", "Jabotinsky 16, Herzliya, Israel",
+    "Rabin Square 4, Kfar Saba, Israel", "Palmach 12, Ashdod, Israel", "HaShalom 8, Ashkelon, Israel",
+    "Bialik 23, Rehovot, Israel", "HaTmarim 2, Eilat, Israel", "Derech Efrat 7, Giv'atayim, Israel",
+    "Herzl 15, Rishon LeZion, Israel", "Balfour 6, Bat Yam, Israel", "HaNassi 3, Ramat HaSharon, Israel",
+    "Derech HaShalom 5, Modiin, Israel", "Einstein 11, Tel Aviv, Israel", "Katzanelson 17, Ramat Gan, Israel",
+    "King George 29, Tel Aviv, Israel", "Allenby 43, Tel Aviv, Israel", "Nachalat Binyamin 12, Tel Aviv, Israel",
+    "HaShalom 15, Tel Aviv, Israel", "Ben Gurion 18, Herzliya, Israel", "Rabin Blvd 50, Ramat Hasharon, Israel",
+    "Shenkar 4, Ra'anana, Israel", "Rav Ashi 13, Jerusalem, Israel", "Shlomzion 21, Jerusalem, Israel",
+    "Hillel St 9, Jerusalem, Israel", "Shai Agnon 6, Jerusalem, Israel", "Hadarim 2, Haifa, Israel",
+    "HaTamarim 3, Be'er Sheva, Israel", "Keren Hayesod 7, Ashdod, Israel", "HaRakevet 10, Rishon LeZion, Israel",
+    "Tchernichovsky 14, Tel Aviv, Israel", "Yehuda HaLevi 33, Tel Aviv, Israel", "Nahmani 10, Tel Aviv, Israel",
+    "Brenner 12, Tel Aviv, Israel", "HaShalom 22, Tel Aviv, Israel"
+};
+
+        double[] Latitude = { 3.2,5.4 /*to do*/};
+        double[] Longitude = { 7.2,1.5 /* to do*/};
         Random rand = new Random();
        for (int i=0;i<15;i++)
         {
@@ -89,14 +107,71 @@ private static void createVolunteer()
 
 
         }
-    } 
+    }
     private static void createsCall()
     {
         Random rand = new Random();
 
-        string[] callAddresses = { /* רשימה של 50 כתובות */ };
-        double[] callLatitudes = { /* 50 קווי רוחב */ };
+        string[] callAddresses = {"10 Jaffa Street, Jerusalem",
+    "20 Emek Refaim Street, Jerusalem",
+    "30 Agripas Street, Jerusalem",
+    "40 Shmuel HaNavi Street, Jerusalem",
+    "50 HaNevi'im Street, Jerusalem",
+    "60 Gaza Street, Jerusalem",
+    "4 Herzl Boulevard, Jerusalem",
+    "40 Masliansky Street, Jerusalem",
+    "10 Ben Yehuda Street, Jerusalem",
+    "150 Hebron Road, Jerusalem",
+    "5 Hillel Street, Jerusalem",
+    "6 Korazin Street, Jerusalem",
+    "35 King George Street, Jerusalem",
+    "78 Ben Zvi Street, Jerusalem",
+    "4 Devorah HaNeviah Street, Jerusalem",
+    "15 Yehuda HaNasi Street, Jerusalem",
+    "5 Aliash Street, Jerusalem",
+    "15 Arlozorov Street, Jerusalem",
+    "13 Shamai Street, Jerusalem",
+    "10 Tavor Street, Jerusalem",
+    "55 Ussishkin Street, Jerusalem",
+    "7 Helene HaMalka Street, Jerusalem",
+    "14 Lincoln Street, Jerusalem",
+    "40 Zerach Street, Jerusalem",
+    "22 Mea Shearim Street, Jerusalem",
+    "10 King David Street, Jerusalem",
+    "30 Ruth Street, Jerusalem",
+    "9 Beit HaDfus Street, Jerusalem",
+    "5 David Yellin Street, Jerusalem",
+    "8 Shmuel HaNagid Street, Jerusalem",  
+    "15 HaTzanhanim Street, Jerusalem",
+    "9 Rabbi Akiva Street, Jerusalem",
+    "7 Greenberg Street, Jerusalem",
+    "6 Henrietta Szold Street, Jerusalem",
+    "5 Shmuel HaNagid Street, Jerusalem",
+    "18 Beit Vagan Street, Jerusalem",
+    "12 Neve Sha'anan Street, Jerusalem",
+    "3 Rachel Imenu Street, Jerusalem",
+    "15 Hillel Street, Jerusalem",
+    "8 Bezalel Street, Jerusalem",
+    "9 Kiryat Moshe Street, Jerusalem",
+    "13 Pierre Koenig Street, Jerusalem",
+    "24 Rabbi Herzog Street, Jerusalem",
+    "5 Ein Gedi Street, Jerusalem",
+    "8 HaOman Street, Jerusalem",
+    "5 Ibn Shaprut Street, Jerusalem",
+    "25 Bar Ilan Street, Jerusalem",
+    "6 Yitzhak Kariv Street, Jerusalem",
+    "17 Weizmann Street, Jerusalem",
+    "9 Shmuel HaNagid Street, Jerusalem",
+    "7 Ben Ze'ev Street, Jerusalem",
+    "10 Rivlin Street, Jerusalem"
+};
+
+        //לבדוק שוני בין ההגדרות ביצירת המערך
+    double[] callLatitudes = { /* 50 קווי רוחב */ };
+        double[] longi = new double[]  { 31.78130811991647, 31.76543961779154, 31.783470280009738,31.791048846509597,31.78391107912798, 31.771053788166768, 31.785777797665002, 31.817876781912197, 31.78186936361579, 31.74578724034863, 31.78068910546481, 31.78255160178496, 31.779769, 31.7798566, 31.789588, 31.755770, 31.755974, 31.781584, 31.772610, 31.781239, 31.782361, 31.780486, 31.7820255, 31.775421, 31.816719, 31.787706, 31.776943, 31.7765005, 31.785952, 31.786426, 31.780594, 31.779808, 31.816370, 31.779226, 31.817092, 31.757365, 31.780274, 31.771251, 31.771493, 31.763331, 31.780627, 31.780728, 31.786061, 31.756843, 31.769767, 31.753902, 31.748810, 31.776448, 31.794707, 31.777475, 31.790191, 31.779852, 31.780946 };
+
         double[] callLongitudes = { /* 50 קווי אורך */ };
+        double[] lati = new double[] { 35.22119546257982, 35.22123577032626,  35.215657362641764,  35.224665335668945, 35.221665622205705, 35.21205714918638, 35.19764122028971, 35.18968763754008, 35.21774235102746, 35.21658163568471, 35.21638617800225, 35.209312878017535, 35.2159379, 35.2092867, 35.224894, 35.202146, 35.201317, 35.215236, 35.216455, 35.218053, 35.213336, 35.212081, 35.221056, 35.219964, 35.190149, 35.222337, 35.222155, 35.217356, 35.189180, 35.216914, 35.214816, 35.226296, 35.192176, 35.218002, 35.191500, 35.160694, 35.214955, 35.183917, 35.200909, 35.218213, 35.217068, 35.214544, 35.195686, 35.215021, 35.208627, 35.221349, 35.211373, 35.211675, 35.218078, 35.224944, 35.198722, 35.214792, 35.220409};
 
         int idIndex = 0;
         foreach (var address in callAddresses)
